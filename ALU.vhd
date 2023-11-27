5bcdf9fd54cf144a1a351bfa32d41442bc056e3c
kaokb <kaokabkareem@gmail.com>
2023-11-27 03:29:38 +0200

    refactor ALU

--- ALU.vhd
+++ ALU.vhd
@@ -17,8 +17,9 @@ entity ALU is
 end entity;
 
 architecture archALU of ALU is
-  signal Imm : natural range 0 to 31;
+  signal Imm : std_logic_vector(31 downto 0);
 begin
+  -- Imm <= (to_integer(unsigned(Reg2)) => '1', others => '0');
   process (clk, reset, Signals)
     variable tempOut : std_logic_vector(31 downto 0) := (others => '0');
     variable tempCCR : std_logic_vector(3 downto 0)  := (others => '0');
@@ -29,6 +30,10 @@ begin
     else
       case Signals is
         when "0000" =>
+          -- NOP
+          tempOut := (others => '0');
+          tempCCR := CCR;
+        when "0001" =>
           -- NOT
           if rising_edge(clk) then
             tempOut := not Reg1;
@@ -37,7 +42,7 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "0001" =>
+        when "0010" =>
           -- NEG
           if rising_edge(clk) then
             tempOut := 0 - Reg1;
@@ -46,7 +51,7 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "0010" =>
+        when "0011" =>
           -- INC
           if rising_edge(clk) then
             tempOut := Reg1 + 1;
@@ -55,7 +60,7 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "0011" =>
+        when "0100" =>
           -- DEC
           if rising_edge(clk) then
             tempOut := Reg1 - 1;
@@ -64,12 +69,12 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "0100" =>
+        when "0101" =>
           -- SEWP
           if rising_edge(clk) then
             -- SWAP
           end if;
-        when "0101" =>
+        when "0110" =>
           -- ADD
           if rising_edge(clk) then
             tempOut := Reg1 + Reg2;
@@ -78,7 +83,7 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "0110" =>
+        when "0111" =>
           -- ADDI
           if rising_edge(clk) then
             tempOut := Reg1 + Reg2;
@@ -87,7 +92,7 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "0111" =>
+        when "1000" =>
           -- SUB
           if rising_edge(clk) then
             tempOut := Reg1 - Reg2;
@@ -96,7 +101,7 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "1000" =>
+        when "1001" =>
           -- AND
           if rising_edge(clk) then
             tempOut := Reg1 and Reg2;
@@ -105,7 +110,7 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "1001" =>
+        when "1010" =>
           -- OR
           if rising_edge(clk) then
             tempOut := Reg1 or Reg2;
@@ -114,7 +119,7 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "1010" =>
+        when "1011" =>
           -- XOR
           if rising_edge(clk) then
             tempOut := Reg1 xor Reg2;
@@ -123,7 +128,7 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "1011" =>
+        when "1100" =>
           -- CMP
           if rising_edge(clk) then
             tempOut := Reg1 - Reg2;
@@ -132,18 +137,18 @@ begin
             end if;
             tempCCR(1) := tempOut(tempOut'left);
           end if;
-        when "1100" =>
-          -- BITSET
-          if rising_edge(clk) then
-            if Reg2 >= 0 and Reg2 <= 31 then
-              -- Imm <= to_integer(Reg2(15 downto 0));
-              tempCCR(2) := Reg1(imm);
-              tempOut(imm) := '1';
-            else
-              tempCCR(2) := '0';
-            end if;
-          end if;
         when "1101" =>
+          -- BITSET
+          -- if rising_edge(clk) then
+          --   if Reg2 >= 0 and Reg2 <= 31 then
+          --     -- Imm <= to_integer(Reg2(15 downto 0));
+          --     tempCCR(2) := Reg1(imm);
+          --     tempOut(imm) := '1';
+          --   else
+          --     tempCCR(2) := '0';
+          --   end if;
+          -- end if;
+        when "1110" =>
           -- RCL
           if rising_edge(clk) then
             if tempOut = "00000000000000000000000000000000" then
@@ -151,24 +156,23 @@ begin
             end if;
             tempCCR(2) := Reg1(0);
           end if;
-        when "1110" =>
+        when "1111" =>
           -- RCR
           if rising_edge(clk) then
             --imm no set
             tempOut := (tempCCR(2) & Reg1(31 downto 1));
             tempCCR(2) := Reg1(0);
           end if;
-        when "1111" =>
-          -- LDM
-          if rising_edge(clk) then
-            tempOut := Reg2;
-            if tempOut = "00000000000000000000000000000000" then
-              tempCCR(0) := '1';
-            end if;
-            tempCCR(1) := tempOut(tempOut'left);
-          end if;
- when others =>
-   tempCCR(0) := '1';
+          -- when "1111" =>
+          --   -- LDM
+          --   if rising_edge(clk) then
+          --     tempOut := Reg2;
+          --     if tempOut = "00000000000000000000000000000000" then
+          --       tempCCR(0) := '1';
+          --     end if;
+          --     tempCCR(1) := tempOut(tempOut'left);
+          --   end if;
+        when others => tempCCR(0) := '1';
       end case;
     end if;
     CCROut <= tempCCR;
