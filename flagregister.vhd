a8722de5b0a760ccd865218b2620766ed3004275
YaraHisham61 <yara.mahrous01@eng-st.cu.edu.eg>
2023-11-30 16:44:09 +0200

    added pipline and flag reg

---
+++ flagregister.vhd
@@ -0,0 +1,31 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+-- WE --> Write Enable
+ENTITY flagregister IS
+    PORT (
+        clk : IN STD_LOGIC;
+        rst : IN STD_LOGIC;
+        WE : IN STD_LOGIC;
+        inp : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+        outp : OUT STD_LOGIC_VECTOR(3 DOWNTO 0)
+    );
+END flagregister;
+
+ARCHITECTURE flagarch OF flagregister IS
+    SIGNAL enable : STD_LOGIC := '1';
+    SIGNAL temp : STD_LOGIC_VECTOR(3 DOWNTO 0) := (OTHERS => '0');
+BEGIN
+    WITH WE SELECT
+        temp <= inp WHEN '1',
+        temp WHEN OTHERS;
+    dffs : FOR i IN 3 DOWNTO 0 GENERATE
+        dff : ENTITY work.DFF
+            PORT MAP(
+                clk => clk,
+                rst => rst,
+                en => enable,
+                D => temp(i),
+                Q => outp(i)
+            );
+    END GENERATE;
+END ARCHITECTURE;
