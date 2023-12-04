3bc8daa5dde2b7a930ca3af7bf81913fc3c34241
kaokb <kaokabkareem@gmail.com>
2023-12-04 12:57:32 +0200

    modify signExt

--- sign_extension.vhd
+++ sign_extension.vhd
@@ -4,6 +4,7 @@ USE ieee.numeric_std.ALL;
 
 ENTITY sign_extension IS
     PORT (
+        en: IN STD_LOGIC;
         in_num : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
         out_num : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
     );
@@ -13,9 +14,11 @@ ARCHITECTURE SignExtendArch OF sign_extension IS
 signal signed_16bit : signed(15 downto 0);
 signal signed_32bit : signed(31 downto 0);
 BEGIN 
+
     signed_16bit <= signed(in_num);
     signed_32bit <= resize(signed_16bit, signed_32bit'length);
-    out_num <= std_logic_vector(signed_32bit);
+    out_num <= std_logic_vector(signed_32bit) WHEN en = '1';
+
 END SignExtendArch;
 
 
