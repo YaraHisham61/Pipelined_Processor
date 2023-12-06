759d00746e79a02859a47c761a4cb07fa309fbaa
kaokb <kaokabkareem@gmail.com>
2023-12-06 11:15:10 +0200

    done

--- ALU.vhd
+++ ALU.vhd
@@ -31,42 +31,40 @@ begin
   ones     <= (0 => '1', others => '0');
   tempReg1 <= Reg1(31) & Reg1;
   tempReg2 <= Reg2(31) & Reg2;
-  tempOut  <= not tempReg1          when Signals = "0001" else
-              0 - tempReg1          when Signals = "0010" else
-              tempReg1 + 1          when Signals = "0011" else
-              tempReg1 - 1          when Signals = "0100" else
-              tempReg1 + tempReg2   when Signals = "0110" else
-              tempReg1 + tempReg2   when Signals = "0111" else
-              tempReg1 - tempReg2   when Signals = "1000" else
-              tempReg1 and tempReg2 when Signals = "1001" else
-              tempReg1 or tempReg2  when Signals = "1010" else
-              tempReg1 xor tempReg2 when Signals = "1011" else
-              tempReg1 - tempReg2   when Signals = "1100" else
+  tempOut  <=                                                 not tempReg1          when Signals = "0001" else
+                                                              zeros - tempReg1      when Signals = "0010" else
+                                                              tempReg1 + ones       when Signals = "0011" else
+                                                              tempReg1 - ones       when Signals = "0100" else
+                                              (others => '0') when Signals = "0101" else
+  tempReg1 + tempReg2                                                               when Signals = "0110" else
+                                                              tempReg1 + tempReg2   when Signals = "0111" else
+                                                              tempReg1 - tempReg2   when Signals = "1000" else
+                                                              tempReg1 and tempReg2 when Signals = "1001" else
+                                                              tempReg1 or tempReg2  when Signals = "1010" else
+                                                              tempReg1 xor tempReg2 when Signals = "1011" else
+                                                              tempReg1 - tempReg2   when Signals = "1100" else
                                                   (others => '0');
 
-  aluu: process (clk)
+  aluu: process (Signals, Reg1, Reg2, CCR, tempOut)
   begin
-    if clk'event and clk = '1' then
-      CCROut(0) <= '1' when tempOut(31 downto 0) = "00000000000000000000000000000000"
-    else
-      '0';
-      CCROut(1) <= tempOut(31);
-      CCROut(3) <= '0';
-      case Signals is
-        when "0011" =>
-          CCROut(2) <= tempOut(32);
-        when "0100" =>
-          CCROut(2) <= tempOut(32);
-        when "0101" =>
-          CCROut(2) <= tempOut(32);
-        when "0110" =>
-          CCROut(2) <= tempOut(32);
-        when "0111" =>
-          CCROut(2) <= tempOut(32);
-        when others => CCROut(2) <= '0';
-      end case;
-      RegOut <= tempOut(31 downto 0);
-    end if;
-
+    CCROut(0) <= '1' when tempOut(31 downto 0) = "00000000000000000000000000000000"
+  else
+    '0';
+    CCROut(1) <= tempOut(31);
+    CCROut(3) <= '0';
+    case Signals is
+      when "0011" =>
+        CCROut(2) <= tempOut(32);
+      when "0100" =>
+        CCROut(2) <= tempOut(32);
+      when "0101" =>
+        CCROut(2) <= tempOut(32);
+      when "0110" =>
+        CCROut(2) <= tempOut(32);
+      when "0111" =>
+        CCROut(2) <= tempOut(32);
+      when others => CCROut(2) <= '0';
+    end case;
+    RegOut <= tempOut(31 downto 0);
   end process;
 end architecture;
